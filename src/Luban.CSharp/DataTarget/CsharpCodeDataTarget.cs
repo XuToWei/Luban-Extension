cbdf83246b8a442acdc7e69cf3bb6cdb223c648d
XuToWei <xw741619720@qq.com>
2024-02-28 21:16:44 +0800

    nolog重定向错误输出，添加cs-code

---
+++ src/Luban.CSharp/DataTarget/CsharpCodeDataTarget.cs
@@ -0,0 +1,108 @@
+﻿using System.Reflection;
+using System.Text;
+using Luban.CodeFormat;
+using Luban.CodeFormat.CodeStyles;
+using Luban.CodeTarget;
+using Luban.CSharp.CodeTarget;
+using Luban.CSharp.DataVisitors;
+using Luban.CSharp.TemplateExtensions;
+using Luban.CSharp.TypeVisitors;
+using Luban.Datas;
+using Luban.DataTarget;
+using Luban.Defs;
+using Luban.TemplateExtensions;
+using Luban.Tmpl;
+using Luban.Utils;
+using Scriban;
+using Scriban.Runtime;
+
+namespace Luban.CSharp.DataTarget;
+
+[DataTarget("cs-code-data")]
+public class CsharpCodeDataTarget : DataTargetBase
+{
+    public string FileHeader => CommonFileHeaders.AUTO_GENERATE_C_LIKE;
+
+    protected string FileSuffixName => "cs";
+    protected string Name => typeof(CsharpCodeCodeTarget).GetCustomAttribute<CodeTargetAttribute>().Name;
+
+    protected virtual ICodeStyle CodeStyle => _codeStyle ??= CreateConfigurableCodeStyle();
+
+    private ICodeStyle _codeStyle;
+    
+    private ICodeStyle CreateConfigurableCodeStyle()
+    {
+        var baseStyle = GenerationContext.Current.GetCodeStyle(Name) ?? CodeFormatManager.Ins.CsharpDefaultCodeStyle;
+
+        var env = EnvManager.Current;
+        string namingKey = BuiltinOptionNames.NamingConvention;
+        return new OverlayCodeStyle(baseStyle,
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "namespace", true, ""),
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "type", true, ""),
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "method", true, ""),
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "property", true, ""),
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "field", true, ""),
+            env.GetOptionOrDefault($"{namingKey}.{Name}", "enumItem", true, "")
+        );
+    }
+
+    protected TemplateContext CreateTemplateContext(Template template)
+    {
+        var ctx = new TemplateContext()
+        {
+            LoopLimit = 0,
+            NewLine = "\n",
+        };
+        ctx.PushGlobal(new ContextTemplateExtension());
+        ctx.PushGlobal(new TypeTemplateExtension());
+        OnCreateTemplateContext(ctx);
+        return ctx;
+    }
+
+    protected void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        ctx.PushGlobal(new CsharpCodeTemplateExtension());
+    }
+    
+    public void GenerateCodeData(DefTable table, List<Record> records, StringBuilder result)
+    {
+        var template = GetTemplate();
+        var tplCtx = CreateTemplateContext(template);
+        var extraEnvs = new ScriptObject
+        {
+            { "__name", table.Name },
+            { "__namespace", table.Namespace },
+            { "__namespace_with_top_module", table.NamespaceWithTopModule },
+            { "__full_name_with_top_module", table.FullNameWithTopModule },
+            { "__table", table },
+            { "__this", table },
+            { "__key_type", table.KeyTType},
+            { "__value_type", table.ValueTType},
+            { "__code_style", CodeStyle},
+            { "__records", records},
+        };
+        tplCtx.PushGlobal(extraEnvs);
+        result.Append(template.Render(tplCtx));
+    }
+    
+    protected virtual Scriban.Template GetTemplate()
+    {
+        if (TemplateManager.Ins.TryGetTemplate($"{typeof(CsharpCodeCodeTarget).GetCustomAttribute<CodeTargetAttribute>().Name}/tabledata", out var template))
+        {
+            return template;
+        }
+        throw new Exception("template:tabledata not found");
+    }
+    
+    protected override string OutputFileExt => "cs";
+    public override OutputFile ExportTable(DefTable table, List<Record> records)
+    {
+        var ss = new StringBuilder();
+        GenerateCodeData(table, records, ss);
+        return new OutputFile()
+        {
+            File = $"{table.OutputDataFile}.{OutputFileExt}",
+            Content = ss.ToString(),
+        };
+    }
+}
---
+++ src/Luban.CSharp/DataVisitors/ToCsharpCodeLiteralVisitor.cs
@@ -0,0 +1,162 @@
+﻿using System.Text;
+using Luban.CodeFormat;
+using Luban.CSharp.TypeVisitors;
+using Luban.DataLoader;
+using Luban.Datas;
+using Luban.DataVisitors;
+using Luban.Defs;
+using Luban.Utils;
+
+namespace Luban.CSharp.DataVisitors;
+
+public class ToCsharpCodeLiteralVisitor : IDataFuncVisitor<string>
+{
+    public static ToCsharpCodeLiteralVisitor Ins { get; } = new();
+
+    public ICodeStyle CodeStyle = CodeFormatManager.Ins.CsharpDefaultCodeStyle;
+
+    public string Accept(DBool type)
+    {
+        return type.Value ? "true" : "false";
+    }
+
+    public string Accept(DByte type)
+    {
+        return type.Value.ToString();
+    }
+
+    public string Accept(DShort type)
+    {
+        return type.Value.ToString();
+    }
+
+    public string Accept(DInt type)
+    {
+        return type.Value.ToString();
+    }
+
+    public string Accept(DLong type)
+    {
+        return type.Value.ToString();
+    }
+
+    public string Accept(DFloat type)
+    {
+        return $"{type.Value}f";
+    }
+
+    public string Accept(DDouble type)
+    {
+        return type.Value.ToString();
+    }
+
+    public string Accept(DDateTime type)
+    {
+        return type.UnixTimeOfCurrentContext.ToString();
+    }
+
+    public string Accept(DString type)
+    {
+        return $"@\"{type.Value}\"";
+    }
+
+    public string Accept(DBean type)
+    {
+        var x = new StringBuilder();
+        x.Append($"new {type.ImplType.FullNameWithTopModule} (");
+        for (int i = 0; i < type.Fields.Count; i++)
+        {
+            var defField = (DefField)type.ImplType.HierarchyFields[i];
+            if (!defField.NeedExport())
+            {
+                continue;
+            }
+            var f = type.Fields[i];
+            if (f == null)
+            {
+                x.Append("default");
+            }
+            else
+            {
+                x.Append($"{f.Apply(this)}");
+            }
+            if (i < type.Fields.Count - 1)
+            {
+                x.Append(", ");
+            }
+        }
+        x.Append(")");
+        return x.ToString();
+    }
+
+    private void Append(List<DType> datas, StringBuilder x)
+    {
+        x.Append("{ ");
+        foreach (var e in datas)
+        {
+            x.Append(e.Apply(this));
+            x.Append(", ");
+        }
+        x.Append('}');
+    }
+
+    public string Accept(DArray type)
+    {
+        var x = new StringBuilder();
+        x.Append($"new {type.Type.Apply(UnderlyingDeclaringTypeNameVisitor.Ins)} ");
+        Append(type.Datas, x);
+        return x.ToString();
+    }
+
+    public string Accept(DList type)
+    {
+        var x = new StringBuilder();
+        x.Append($"new {type.Type.Apply(UnderlyingDeclaringTypeNameVisitor.Ins)} ");
+        Append(type.Datas, x);
+        return x.ToString();
+    }
+
+    public string Accept(DSet type)
+    {
+        var x = new StringBuilder();
+        x.Append($"new {type.Type.Apply(UnderlyingDeclaringTypeNameVisitor.Ins)} ");
+        Append(type.Datas, x);
+        return x.ToString();
+    }
+
+    public string Accept(DMap type)
+    {
+        var x = new StringBuilder();
+        x.Append($"new {type.Type.Apply(UnderlyingDeclaringTypeNameVisitor.Ins)} {{ ");
+        foreach (var e in type.Datas)
+        {
+            x.Append($"[ {e.Key.Apply(this)} ] = {e.Value.Apply(this)}, ");
+        }
+        x.Append('}');
+        return x.ToString();
+    }
+
+    public string Accept(DEnum type)
+    {
+        if (string.IsNullOrEmpty(type.StrValue) || string.Equals(type.StrValue, "0"))
+            return "default";
+        var x = new StringBuilder();
+        string[] enums = type.StrValue.Split('|');
+        for (int i = 0; i < enums.Length; i++)
+        {
+            foreach (var enumItem in type.Type.DefEnum.Items)
+            {
+                if (enumItem.Name == enums[i] || enumItem.Value == enums[i] || enumItem.Alias == enums[i])
+                {
+                    x.Append($"{type.Type.Apply(UnderlyingDeclaringTypeNameVisitor.Ins)}.{enumItem.Name}");
+                    break;
+                }
+            }
+            if (i != enums.Length - 1)
+            {
+                x.Append(" | ");
+            }
+        }
+        return x.ToString();
+    }
+}
